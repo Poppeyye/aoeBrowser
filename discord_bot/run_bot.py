4bd86b2d84e9fe5a8e544742088891a61ed069cf
Poppeyye <alvaro.martinfidalgo@gmail.com>
2023-08-28 14:58:45 +0200

    include all modules to package

--- discord_bot/run_bot.py
+++ discord_bot/run_bot.py
@@ -1,6 +1,9 @@
 import os
 import discord
+import sys
 
+package_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(package_dir, ".."))
 from scraper.lobbies import AoeRORLobbyBrowser
 
 TOKEN = os.getenv("BOT_TOKEN")
